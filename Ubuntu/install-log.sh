18ee73b184fb1fc7273a0b0d3a9ea2e9dbe38de8
Sean Sosik-Hamor <sean@alphadefense.com>
2021-01-04 16:33:33 -0500

    Added install-log script.

---
+++ Ubuntu/install-log.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# TODO: Check for symlink attack.
+
+APTLOG=$HOME/Documents/install-apt-log
+SNAPLOG=$HOME/Documents/install-snap.log
+LOGTMP=$(mktemp)
+
+touch $APTLOG $SNAPLOG
+
+sort -u $APTLOG > $LOGTMP
+cat $HOME/.bash_history | grep "apt install" | grep -v grep | awk '{print $4}' | sort -u >> $LOGTMP
+sort -u $LOGTMP > $APTLOG
+
+sort -u $SNAPLOG > $LOGTMP
+snap list | grep -v Name | awk '{print $1}' >> $LOGTMP
+sort -u $LOGTMP > $SNAPLOG
