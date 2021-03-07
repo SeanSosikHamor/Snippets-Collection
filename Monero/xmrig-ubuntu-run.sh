0445518a40062fbad0b8d0b4522135f0c4328210
Sean Sosik-Hamor <sean@hamor.com>
2021-03-07 13:02:36 -0500

    Added CPU priority and API.

--- Monero/xmrig-ubuntu-run.sh
+++ Monero/xmrig-ubuntu-run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-./xmrig -o monerohash.com:9999 -u 8B4iaesxRqbhKYDJMqbx1HGb1522tqe3EhceXDFBkXFXaVae7muQ3rF7c9rXuU7X3gdeooY7SquwuBRkb8o24Mt1PT5utHM -k --tls
+./xmrig -o monerohash.com:9999 \
+    -u 8B4iaesxRqbhKYDJMqbx1HGb1522tqe3EhceXDFBkXFXaVae7muQ3rF7c9rXuU7X3gdeooY7SquwuBRkb8o24Mt1PT5utHM \
+    --cpu-priority=0 \
+    --http-host=0.0.0.0 \
+    --http-port=8080 \
+    -k --tls
